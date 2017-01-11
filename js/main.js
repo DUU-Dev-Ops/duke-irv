ac8f67882c896d649f1b083816888711e55d58e1
Vishnu Menon <me@vishnumenon.com>
2017-01-10 19:28:26 -0500

    Finished major features & ui

--- js/main.js
+++ js/main.js
@@ -1,5 +1,123 @@
-function processVotingData(data) {
-    console.log(data);
+var app = new Vue({
+    el: '#app',
+    data: {
+        positions: [],
+        allResults: {},
+        currPos: '',
+        results: {},
+        voteCount: 0
+    },
+    methods: {
+        setCurrPos: function(position) {
+            this.currPos = position;
+            this.results = this.allResults[position];
+        },
+        downloadVotes: function() {
+            var ctx = this;
+            var voteArray = this.results.validVotes.map(function(vote) {
+                return vote[ctx.currPos];
+            });
+            console.log(voteArray);
+            var csv = Papa.unparse(voteArray);
+            download(csv, this.currPos.replace(/\W/g, '') + ".csv", "text/csv")
+        }
+    }
+})
+
+function processVotingData(votes, positions) {
+    var positionsArray = Object.keys(positions);
+    var allResults = {};
+    positionsArray.forEach(function(position) {
+        var res = {};
+        res.validVotes = getValidVotes(votes, position);
+        res.voteCount = res.validVotes.length;
+        res.rounds = [];
+        var r = 1;
+        var tally = {};
+        var eliminated = [];
+        var loser;
+        var chartData;
+        var stackedData = [];
+        do {
+            tally = tallyVotes(res.validVotes, position, eliminated);
+            loser = getLastPlace(tally)
+            eliminated.push(loser);
+            chartData = $.extend({}, tally.candidates);
+            chartData["Exhausted"] = tally.spent;
+            if (r > 1) {
+                var newVotes = {};
+                var oldVotes = {};
+                Object.keys(chartData).forEach(function(k) {
+                    oldVotes[k] = res.rounds[r - 2].chartData[k];
+                    newVotes[k] = chartData[k] - res.rounds[r - 2].chartData[k];
+                })
+                stackedData = [{
+                    name: "Before instant Runoff",
+                    data: oldVotes
+                }, {
+                    name: "New Votes",
+                    data: newVotes
+                }];
+            }
+            res.rounds.push({
+                loser: loser,
+                number: r,
+                chartData: chartData,
+                stackedData: stackedData
+            })
+            r++;
+        } while (!majorityWinner(tally))
+        res.winner = majorityWinner(tally);
+        allResults[position] = res;
+    });
+    app.positions = positionsArray;
+    app.currPos = positionsArray[0];
+    app.allResults = allResults;
+    app.results = allResults[app.currPos];
+    app.voteCount = votes.length;
+}
+
+function getLastPlace(tally) {
+    return Object.keys(tally.candidates).reduce(function(a, b) {
+        return tally.candidates[a] < tally.candidates[b] ? a : b
+    });
+}
+
+function majorityWinner(tally) {
+    var winner = Object.keys(tally.candidates).reduce(function(a, b) {
+        return tally.candidates[a] > tally.candidates[b] ? a : b
+    });
+    var voteSum = Object.keys(tally.candidates).reduce(function(a, b) {
+        return tally.candidates[a] + tally.candidates[b]
+    });
+    if (tally.candidates[winner] / voteSum > 0.5) return winner;
+    return null;
+}
+
+function tallyVotes(votes, position, eliminated) {
+    var tally = {};
+    tally.candidates = {};
+    tally.spent = 0;
+    votes.forEach(function(vote) {
+        let fixedVote = vote[position].filter(function(candidate) {
+            return eliminated.indexOf(candidate) < 0;
+        });
+        if (fixedVote.length > 0) {
+            if (!tally.candidates[fixedVote[0]]) tally.candidates[fixedVote[0]] = 0;
+            tally.candidates[fixedVote[0]]++;
+        } else {
+            tally.spent++;
+        }
+    })
+    return tally;
+}
+
+
+
+function getValidVotes(votes, position) {
+    return votes.filter(function(vote) {
+        return vote[position].length > 0;
+    })
 }
 
 function cleanData(fields, data) {
@@ -8,7 +126,8 @@ function cleanData(fields, data) {
         Object.keys(fields).forEach(function(position) {
             newVote[position] = [];
             for (var i = 1; i <= fields[position]; i++) {
-                newVote[position].push(vote[position + " - " + i]);
+                if (vote[position + " - " + i])
+                    newVote[position].push(vote[position + " - " + i]);
             }
         });
         return newVote;
@@ -33,9 +152,9 @@ function loadHandler(event) {
         header: true,
     }
     var data = Papa.parse(csv, parseConfig);
-    var fields = processFields(data.meta.fields);
-    var votes = cleanData(fields, data.data);
-    processVotingData(votes);
+    var positions = processFields(data.meta.fields);
+    var votes = cleanData(positions, data.data);
+    processVotingData(votes, positions);
 }
 
 function errorHandler(evt) {
